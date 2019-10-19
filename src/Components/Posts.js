09f00c080881b74fb6d7086edb817dc6bba0fbf9
Yassine JOUT <yassinejout@gmail.com>
2019-10-19 11:26:56 +0100

    feat(Reteiveposts): retrieve and display posts

---
+++ src/Components/Posts.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react'
+
+class Posts extends Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            posts : []
+        }
+    }
+    componentWillMount(){
+        fetch("https://jsonplaceholder.typicode.com/posts")
+            .then(res =>  res.json())
+            .then(data => this.setState({posts : data}));
+    }
+    render(){
+        const postItems = this.state.posts.map(
+            elem => (<div key={elem.id}><h2>{elem.title}</h2><p>{elem.body}</p><br/></div>)
+        );
+        return (
+            <div>
+                <h1>Posts</h1>
+                    {
+                        postItems
+                    }
+                
+            </div>
+        );
+    }
+}
+
+export default Posts;
