8666c41fc700bdc78319683f03df11d21ec676d1
Yassine JOUT <yassinejout@gmail.com>
2019-10-24 07:56:29 +0100

    improvment(store): make the store usable at the upper level of the app

--- src/App.js
+++ src/App.js
@@ -1,21 +1,17 @@
 import React from 'react';
-import { Provider } from 'react-redux'
-
 import Posts from './Components/Posts'
 import PostForm from './Components/PostForm'
 
-import store from './Components/Store'
+
 
 
 function App() {
 
   return (
-    <Provider store={store}>
       <div className="App">
         <PostForm />
         <Posts />
     </div>
-    </Provider>
     
   );
 }
